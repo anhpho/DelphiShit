4ca2468018b10445b36732905549555af25873c1
anhpho <anhpho@users.noreply.github.com>
2015-04-03 17:07:36 +0700

    Fixed build failed

--- DelphiProject/ISProject.dpr
+++ DelphiProject/ISProject.dpr
@@ -18,6 +18,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TImportForm, ImportForm);
   Application.Run;
 end.
